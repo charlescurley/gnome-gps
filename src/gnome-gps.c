384c143b8535e6e4fa745cf2955aeeb5df246e1a
Charles Curley <charlescurley@charlescurley.com>
2025-05-06 16:22:38 -0600

    Moved resetting the SATELLITE_SET bit so that the bit mask is correct.

--- src/gnome-gps.c
+++ src/gnome-gps.c
@@ -974,8 +974,6 @@ void showData (void) {
     if (gpsdata.set & SATELLITE_SET) {
         gchar banner[STRINGBUFFSIZE];
 
-        gpsdata.set &= ~(SATELLITE_SET);
-
         /* Guard against division of or by 0. */
         if (gpsdata.satellites_visible > 0 && gpsdata.satellites_used > 0) {
             /* We have at least one satellite visible and at least one
@@ -1013,6 +1011,7 @@ void showData (void) {
             (void) printf ("set 0x%08x, %s\n", (unsigned int) gpsdata.set,
                            banner);
         }
+        gpsdata.set &= ~(SATELLITE_SET);
         return;
     }
 
